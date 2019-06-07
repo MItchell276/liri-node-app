69d48614d73d21dc5d4d549ac29fdd9f410dc27d
Dan MItchell <danmitchell@Dans-MacBook-Pro.local>
2019-06-06 21:44:40 -0400

    updated liri

--- liri.js
+++ liri.js
@@ -1,6 +1,12 @@
 require("dotenv").config()
 var Spotify = require('node-spotify-api');
 var keys = require("./keys")
+var axios = require('axios')
+var moment = require('moment')
+moment().format();
+
+var movieQuery = "lion king"
+var bandQuery = "Gap band"
 
 var command = process.argv[2];
 var input = process.argv[3];
@@ -8,7 +14,9 @@ var input = process.argv[3];
 if (command === "spotify-this-song") {
     spotifyThisSong(input)
 } else if (command === "movie-this") {
-
+    movieThis(input)
+} else if (command === "band-this") {
+    bandThis(input)
 }
 
 function spotifyThisSong(musicSearch) {
@@ -20,22 +28,78 @@ function spotifyThisSong(musicSearch) {
     spotify.search({ type: 'track', query: musicSearch }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
-        }
 
-        console.log(JSON.stringify(data, null, 2));
+        }
+        console.log(JSON.stringify(data, null, 2))
+        console.log(data.track.items[0].artists)
+
+
     });
 
 }
+function bandThis(bandQuery) {
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + bandQuery + "/events?app_id=codingbootcamp";
+    console.log();
+
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log(response.data)
+            console.log("venue name: " + response.data[0].venue.name);
+            console.log("venuecity:" + response.data[0].venue.city);
+            console.log("datetime:" + moment.data.datetime).format("MM/DD/YYYY");
+
+        }
+    );
+}
+
+
+
+
 
 function movieThis(movieQuery) {
-    axios.get('/user?ID=12345')
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    if (movieQuery === undefined || null) {
+        movieQuery = "Mr.Nobody";
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieQuery + "&apikey=5c60fd8d";
+
+    // queryUrl
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function (response) {
+            console.log("Release Year: " + response.data.Year);
+            console.log(response.data)
+            console.log("Title :" + response.data.Title);
+            console.log("IMDB :" + response.data.imdbRating);
+            console.log("Rotten Tomatoes :" + response.data.Ratings[1].Value);
+            console.log("Country :" + response.data.Country);
+            console.log("Language :" + response.data.Language);
+            console.log("Actors :" + response.data.Actors);
+            console.log("Movie Plot :" + response.data.Plot);
+        }
+    );
+
 }
+
+
+
+
+// request(queryUrl, function (error, response, body) {
+//     // hopefully it works
+//     if (err && response.statusCode === 100) {
+//         let concertData = JSON.parse(body);
+
+//         let concertDT = concertData[0].datetime
+//         let momentDT = moment().format('l')
+
+
+//     }
+
+
+// });
+
+
 
 
 
